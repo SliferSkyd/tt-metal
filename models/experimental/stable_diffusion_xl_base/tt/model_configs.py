405fffc8c26d5be45a1a23aa7ab1cd8930b58f6f
ppopovic <ppopovic@tenstorrent.com>
2025-07-21 21:42:21 +0000

    #0: test working

--- models/experimental/stable_diffusion_xl_base/tt/model_configs.py
+++ models/experimental/stable_diffusion_xl_base/tt/model_configs.py
@@ -1025,13 +1025,17 @@ class ModelOptimisations:
 
             # UP BLOCK 1
             elif "up_blocks.1.resnets.0.conv1" == conv_path:
+                # same as in tp
                 return self.conv_configs["ABH_32_NO_ADB_BS"]
             elif "up_blocks.1.resnets.1.conv1" == conv_path:
                 return self.conv_configs["ABH_64_NO_ADB_WDB_BS"]
             elif "up_blocks.1.resnets.2.conv1" == conv_path:
                 return self.conv_configs["ABH_64_NO_ADB_WDB_MOVE_BS"]
             elif ("up_blocks.1.resnets" in conv_path) and ("conv2" in conv_path):
-                return self.conv_configs["ABH_128_ADB_WDB_BS"]
+                if parallelism_strategy == SdxlParallelism.NoParallelism:
+                    return self.conv_configs["ABH_128_ADB_WDB_BS"]
+                else:
+                    return self.conv_configs["ABH_128_ADB_WDB_BS_DB1_TP2"]
             elif "up_blocks.1.upsamplers.0" == conv_path:
                 return self.conv_configs["ABH_64_NO_ADB_WDB_BS"]
 
