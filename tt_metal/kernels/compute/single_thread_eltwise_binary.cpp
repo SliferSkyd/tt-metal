021a28aa550ad3b17d226eb48fc936ade8fe80cb
Anil Mahmud <amahmud@tenstorrent.com>
2025-07-29 04:10:11 +0000

    Moved reader code to compute kernel, having runtime error

--- tt_metal/kernels/compute/single_thread_eltwise_binary.cpp
+++ tt_metal/kernels/compute/single_thread_eltwise_binary.cpp
@@ -31,13 +31,38 @@ void MAIN {
     constexpr auto cb_in1 = tt::CBIndex::c_1;
     constexpr auto cb_out0 = tt::CBIndex::c_16;
 
+    // single-tile ublocks
+    uint32_t ublock_size_bytes_0 = get_tile_size(cb_in0);
+    uint32_t ublock_size_bytes_1 = get_tile_size(cb_in1);
+
+    uint32_t l1_write_addr_in0;
+    uint32_t l1_write_addr_in1;
+
     // Initialize the parts that are common among binary operations
     binary_op_init_common_st(cb_in0, cb_in1, cb_out0);
 
     // Initialize the parts that required specifically for this binary operatoins
     binary_tiles_init_st<false, EltwiseBinaryType::ELWADD>(cb_in0, cb_in1);
 
-    for (uint32_t block = 0; block < per_core_block_cnt; ++block) {
+    for (uint32_t block = 0; block < per_core_block_cnt; block += per_core_block_size) {
+        // Read a tile to cb_in0 from DRAM
+        uint64_t src0_noc_addr = get_noc_addr_from_bank_id<true>(src0_bank_id, src0_addr);
+        cb_reserve_back(cb_in0, per_core_block_size);
+        l1_write_addr_in0 = get_write_ptr(cb_in0);
+        noc_async_read(src0_noc_addr, l1_write_addr_in0, ublock_size_bytes_0);
+        noc_async_read_barrier();
+        cb_push_back(cb_in0, per_core_block_size);
+        src0_addr += ublock_size_bytes_0;
+
+        // Read a tile to cb_in0 from DRAM
+        uint64_t src1_noc_addr = get_noc_addr_from_bank_id<true>(src1_bank_id, src1_addr);
+        cb_reserve_back(cb_in1, per_core_block_size);
+        l1_write_addr_in1 = get_write_ptr(cb_in1);
+        noc_async_read(src1_noc_addr, l1_write_addr_in1, ublock_size_bytes_1);
+        noc_async_read_barrier();
+        cb_push_back(cb_in1, per_core_block_size);
+        src1_addr += ublock_size_bytes_1;
+
         // Wait for the input circular buffers to be filled with per_core_block_size tiles
         cb_wait_front(cb_in0, per_core_block_size);
         cb_wait_front(cb_in1, per_core_block_size);
