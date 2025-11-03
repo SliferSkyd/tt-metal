a98a57602bf47fb6c8d0c8f8faafd7a1ae33b88e
Pavle Josipovic <pjosipovic@tenstorrent.com>
2025-11-03 09:59:19 +0000

    more optimisations

--- models/experimental/panoptic_deeplab/tests/test_device_perf_pdl.py
+++ models/experimental/panoptic_deeplab/tests/test_device_perf_pdl.py
@@ -21,7 +21,7 @@ from models.experimental.panoptic_deeplab.reference.pytorch_model import DEEPLAB
         # ),
         (
             "pytest models/experimental/panoptic_deeplab/tests/pcc/test_tt_model.py::test_model_panoptic_deeplab -k test_deeplab_v3_plus",
-            30_874_852,
+            25_065_032,
             DEEPLAB_V3_PLUS,
             DEEPLAB_V3_PLUS,
             1,
