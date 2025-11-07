05cea35c19780414bb4aa7a62bea1355b21f38fd
edwinleeTT <edwinlee@tenstorrent.com>
2025-11-07 21:18:06 +0000

    Conv2d change had second unqualified optional deref

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d.cpp
@@ -735,7 +735,7 @@ Result conv2d_L1(
             mm_output_memory_config,
             output_dtype,
             program_config,
-            conv_config.activation.value(),
+            conv_config.activation,
             compute_config);
 
         if (conv_config.deallocate_activation) {
